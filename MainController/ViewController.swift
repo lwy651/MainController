3ac06b7912c79089dc4d9dccb4d25838c67a2c52
xiao_51 <972766740@qq.com>
2017-06-03 12:56:42 +0800

    Initial Commit

---
+++ MainController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MainController
+//
+//  Created by 651 on 2017/6/3.
+//  Copyright © 2017年 651. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
